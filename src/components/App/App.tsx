8660a18c83739dc0eb35d23c0434b1da909be384
DmitrySaf <dmitry.study@yandex.ru>
2022-09-29 10:37:07 +0300

    added components folder, replaced the components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import Calculator from "../Calculator/Calculator";
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
