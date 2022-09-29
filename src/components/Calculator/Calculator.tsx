26d9fe3e080bb5326b3cb315751d49ea68f74808
DmitrySaf <dmitry.study@yandex.ru>
2022-09-29 15:55:30 +0300

    made adaptive layout, without range slider

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -9,8 +9,10 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <h1 className="calculator__title">Рассчитайте стоимость автомобиля в лизинг</h1>
-      <Inputs />
-      <Results />
+      <form method="POST" action="#" encType="application/x-www-form-urlencoded" className="calculator__form">
+        <Inputs />
+        <Results />
+      </form>
     </div>
   );
 };
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -7,14 +7,14 @@ const Results = () => {
     <div className="results">
       <div className="results__column">
         <div className="results__label">Сумма договора лизинга</div>
-        <div className="results__value">4 467 313 ₽</div>
+        <div className="results__value">4 467 313<div className="results__value-ruble"> ₽</div></div>
       </div>
       <div className="results__column">
         <div className="results__label">Ежемесячный платеж от</div>
-        <div className="results__value">114 455 ₽</div>
+        <div className="results__value">114 455<div className="results__value-ruble"> ₽</div></div>
       </div>
       <div className="results__column">
-        <button className="results__button">Оставить заявку</button>
+        <button type="submit" className="results__button">Оставить заявку</button>
       </div>
     </div>
   );
