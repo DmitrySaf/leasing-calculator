4da970a559094199755ae337173ddbd3f28e1dc9
DmitrySaf <dmitry.study@yandex.ru>
2022-09-29 15:55:14 +0300

    made adaptive layout, without range slider

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -1,29 +1,59 @@
-import React from 'react';
+import { useRef } from 'react';
 
 import './Inputs.scss';
 
 const Inputs = () => {
+  const paymentRef = useRef<HTMLInputElement>(null);
+
+  
+
   return (
     <div className="inputs">
-      <Input labelText="Стоимость автомобиля" id="car-price" unit="₽"/>
-      <Input labelText="Первоначальный взнос" id="payment" unit="13%"/>
-      <Input labelText="Срок лизинга" id="period" unit="мес."/>
+      <div className="input__container">
+        <label htmlFor="car-price" className="input__label">Стоимость автомобиля</label>
+        <div className="input__wrapper">
+          <input
+            name="car-price"
+            type="text"
+            id="car-price"
+            className="input"
+            autoComplete="off"
+          />
+          <div className="input__unit">₽</div>
+        </div>
+      </div>
+      <div className="input__container">
+        <label htmlFor="payment" className="input__label">Первоначальный взнос</label>
+        <div className="input__wrapper">
+          <div onClick={() => paymentRef.current?.focus()} className="input">
+            420 000
+            <div className="input__unit_aligned"> ₽</div>
+          </div>
+          <input
+            name="payment"
+            type="text"
+            id="payment"
+            ref={paymentRef}
+            className="input__unit input__unit_theme_payment"
+            autoComplete="off" 
+          />
+        </div>
+      </div>
+      <div className="input__container">
+        <label htmlFor="period" className="input__label">Срок лизинга</label>
+        <div className="input__wrapper">
+          <input
+            name="period"
+            type="text"
+            id="period"
+            className="input"
+            autoComplete="off"
+          />
+          <div className="input__unit">мес.</div>
+        </div>
+      </div>
     </div>
   );
 };
-
-const Input = ({ id, labelText, unit }: { id: string, labelText: string, unit: string }) => {
-  return (
-    <div className="input__container">
-      <label htmlFor={id} className="input__label">{labelText}</label>
-      <div className="input__wrapper">
-        <div
-          className={"input__unit " + ((id === 'payment') ? "input__unit_filled" : '')}
-        >{unit}</div>
-        <input type="text" id={id} className="input"/>
-      </div>
-    </div>
-  )
-}
 
 export default Inputs;
