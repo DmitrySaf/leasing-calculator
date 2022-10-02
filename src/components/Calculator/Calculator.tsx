c89fbf46ff8b93e2c0bb09ff5e7ef9b3005099e3
DmitrySaf <dmitry.study@yandex.ru>
2022-10-02 18:05:32 +0300

    replaced functions with consts

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -35,18 +35,12 @@ const Calculator = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const { carPrice, firstPayment, period } = state;
-
-  const getMonthPayment = () => {
-    return Math.round(
-      (carPrice * (1 - (firstPayment / 100)))
-        * ((INTEREST_RATE * Math.pow(1 + INTEREST_RATE, period))
-        / (Math.pow(1 + INTEREST_RATE, period) - 1))
-    );
-  };
-
-  const getTotalSum = () => {
-    return Math.round(firstPayment + period * getMonthPayment());
-  }
+  const monthPay = Math.round(
+    (carPrice * (1 - (firstPayment / 100)))
+      * ((INTEREST_RATE * Math.pow(1 + INTEREST_RATE, period))
+      / (Math.pow(1 + INTEREST_RATE, period) - 1))
+  );
+  const totalSum =  Math.round(firstPayment + period * monthPay);
 
   const handleChange = (values: IFormattedState) => {
     setState(values);
@@ -60,8 +54,8 @@ const Calculator = () => {
       method: 'post',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify({
-        totalSum: getTotalSum(),
-        monthPayment: getMonthPayment(),
+        totalSum: monthPay,
+        monthPayment: totalSum,
         firstPayment: firstPayment * carPrice / 100,
         carPrice,
         period
@@ -88,8 +82,8 @@ const Calculator = () => {
           handleChange={handleChange}
         />
         <Results
-          totalSum={bringToFormat(getTotalSum())}
-          monthPayment={bringToFormat(getMonthPayment())}
+          totalSum={bringToFormat(totalSum)}
+          monthPayment={bringToFormat(monthPay)}
           loading={loading}
           error={error}
         />
